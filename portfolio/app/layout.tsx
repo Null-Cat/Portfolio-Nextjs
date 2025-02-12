aa8b4eb0ca4bc478f144b6d5992aa2d04f6deae1
Philip White <philip@philipwhite.dev>
2025-02-12 20:36:34 +0000

    Add HeroUI Component Library

--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -1,21 +1,24 @@
+import { Providers } from "./providers";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Philip White Portfolio",
   description:
     "I'm a Game Programmer Graduate from Falmouth University with proficient experience in game engines such as Unreal Engine and Unity using languages such as C++ and C#. I am an aspiring Gameplay Programmer looking for a challenging role to express my skills. In my personal projects, I tinker with web development (backend) node.js such as this site which is deployed on my Raspberry Pi using PM2 and Nginx. I also have experience with javascript game engines such as Phaser.",
+  openGraph: {
+    type: "website",
+    url: "https://philipwhite.dev",
+    title: "Philip White Portfolio",
+    description:
+      "I'm a Game Programmer Graduate from Falmouth University with proficient experience in game engines such as Unreal Engine and Unity using languages such as C++ and C#. I am an aspiring Gameplay Programmer looking for a challenging role to express my skills. In my personal projects, I tinker with web development (backend) node.js such as this site which is deployed on my Raspberry Pi using PM2 and Nginx. I also have experience with javascript game engines such as Phaser.",
+    images: [
+      {
+        url: "https://philipwhite.dev/OGImage.png",
+        alt: "Philip White Portfolio",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -25,10 +28,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
---
+++ portfolio/app/project/endlessvendetta/page.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Endless Vendetta Project",
+  description:
+    "Crime, violence and betrayal runs rampant throughout the solar system. Take on bounties as you hunt to kill the one who murdered your family, and left you for dead.",
+  openGraph: {
+    type: "website",
+    url: "https://philipwhite.dev/endlessvendetta",
+    title: "Endless Vendetta Project",
+    description:
+      "Crime, violence and betrayal runs rampant throughout the solar system. Take on bounties as you hunt to kill the one who murdered your family, and left you for dead.",
+  },
+};
+
+const EndlessVendettaProject = () => {
+  return <div>EndlessVendettaProject</div>;
+};
+
+export default EndlessVendettaProject;

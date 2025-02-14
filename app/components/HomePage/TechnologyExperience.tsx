944a683b77d24eb337887b30992f0d09d9ef9aec
Philip White <philip@philipwhite.dev>
2025-02-14 04:40:41 +0000

    Add Technology Experience Component

---
+++ app/components/HomePage/TechnologyExperience.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { Divider, Image } from "@heroui/react";
+import { RefObject } from "react";
+interface TechnologyExperienceProps {
+  transitionRef: RefObject<HTMLDivElement | null>;
+  hasFadedInCheck: boolean;
+  altText: string;
+  src: string;
+  text: string;
+  subText: string;
+}
+
+const TechnologyExperience = ({
+  transitionRef,
+  hasFadedInCheck,
+  altText,
+  src,
+  text,
+  subText,
+}: TechnologyExperienceProps) => {
+  return (
+    <div
+      ref={transitionRef}
+      className={
+        "overflow-y-hidden flex flex-wrap items-center border border-default-100 p-4 rounded-lg overflow-hidden w-fit space-x-4" +
+        (hasFadedInCheck == true ? " backdrop-blur" : "")
+      }
+    >
+      <Image
+        alt={altText}
+        className="pointer-events-auto"
+        src={src}
+        width={60}
+      />
+      <Divider className="h-10" orientation="vertical" />
+      <div className="flex flex-col">
+        <h2 className="sm:text-3xl text-base font-bold pointer-events-auto">
+          {text}
+        </h2>
+        <p className="sm:text-lg text-sm text-default-500 pointer-events-auto">
+          {subText}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default TechnologyExperience;

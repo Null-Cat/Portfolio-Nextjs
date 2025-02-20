a9e9ba2f91b1c882606345b98785c8add224c600
Philip White <philip@philipwhite.dev>
2025-02-20 19:54:36 +0000

    Update PM2 Config for Public Deploy URL

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -41,7 +41,7 @@ module.exports = {
       // SSH user
       user: "phro",
       // SSH host
-      host: "192.168.0.102",
+      host: "deploy.philipwhite.dev",
       // SSH port
       port: "2222",
       // GIT remote/branch

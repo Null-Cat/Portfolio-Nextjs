b9435cd11f85eed6b7ab370b5ac30bcc450a2194
Philip White <philip@philipwhite.dev>
2025-03-04 23:39:43 +0000

    Update Endless Vendetta Project for Carousel Data

--- app/project/endlessvendetta/page.tsx
+++ app/project/endlessvendetta/page.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import ProjectCarousel from "@/app/components/ProjectCarousel";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -21,7 +21,117 @@ export const metadata: Metadata = {
 };
 
 const EndlessVendettaProject = () => {
-  return <div>EndlessVendettaProject</div>;
+  return (
+    <>
+      <h1 className="flex justify-center sm:text-5xl text-3xl text-center pt-20 pb-4">
+        Endless Vendetta
+      </h1>
+      <ProjectCarousel
+        images={[
+          {
+            image:
+              "https://test.philipwhite.dev/endlessvendetta/EndlessVendettaFullPage.webp",
+            alt: "Endless Vendetta Promotional Image",
+          },
+          {
+            image:
+              "https://test.philipwhite.dev/endlessvendetta/Screenshot0.webp",
+            alt: "Main Menu",
+          },
+          {
+            image:
+              "https://test.philipwhite.dev/endlessvendetta/Screenshot1.webp",
+            alt: "First Mayor Interaction",
+          },
+          {
+            image:
+              "https://test.philipwhite.dev/endlessvendetta/Screenshot2.webp",
+            alt: "Mayor Dialogue",
+          },
+          {
+            image:
+              "https://test.philipwhite.dev/endlessvendetta/Screenshot3.webp",
+            alt: "Mayor Dialogue Choices",
+          },
+          {
+            image:
+              "https://test.philipwhite.dev/endlessvendetta/Screenshot4.webp",
+            alt: "Waypoint",
+          },
+          {
+            image:
+              "https://test.philipwhite.dev/endlessvendetta/Screenshot5.webp",
+            alt: "Shooting Gallery",
+          },
+          {
+            image:
+              "https://test.philipwhite.dev/endlessvendetta/Screenshot6.webp",
+            alt: "Combat Gadget",
+          },
+          {
+            image:
+              "https://test.philipwhite.dev/endlessvendetta/Screenshot7.webp",
+            alt: "Using Combat Gadget",
+          },
+          {
+            image:
+              "https://test.philipwhite.dev/endlessvendetta/Screenshot8.webp",
+            alt: "Recon Gadget",
+          },
+          {
+            image:
+              "https://test.philipwhite.dev/endlessvendetta/Screenshot9.webp",
+            alt: "Using Recon Gadget",
+          },
+          {
+            image:
+              "https://test.philipwhite.dev/endlessvendetta/Screenshot12.webp",
+            alt: "New Miami",
+          },
+          {
+            image:
+              "https://test.philipwhite.dev/endlessvendetta/Screenshot10.webp",
+            alt: "New Miami",
+          },
+          {
+            image:
+              "https://test.philipwhite.dev/endlessvendetta/Screenshot11.webp",
+            alt: "New Miami",
+          },
+          {
+            image:
+              "https://test.philipwhite.dev/endlessvendetta/Screenshot13.webp",
+            alt: "New Miami Park",
+          },
+          {
+            image:
+              "https://test.philipwhite.dev/endlessvendetta/Screenshot14.webp",
+            alt: "Side Bounty",
+          },
+          {
+            image:
+              "https://test.philipwhite.dev/endlessvendetta/Screenshot15.webp",
+            alt: "Weapon Attachment Workbench",
+          },
+          {
+            image:
+              "https://test.philipwhite.dev/endlessvendetta/Screenshot16.webp",
+            alt: "Enemy Wave",
+          },
+          {
+            image:
+              "https://test.philipwhite.dev/endlessvendetta/Screenshot17.webp",
+            alt: "Dinning Room",
+          },
+          {
+            image:
+              "https://test.philipwhite.dev/endlessvendetta/Screenshot18.webp",
+            alt: "Getting Damaged",
+          },
+        ]}
+      />
+    </>
+  );
 };
 
 export default EndlessVendettaProject;

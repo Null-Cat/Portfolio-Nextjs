b052003662a0b483972f56ab427331fff18ee33d
Philip White <philip@philipwhite.dev>
2025-03-05 00:31:44 +0000

    Update Endless Vendetta for Feature List & Description

--- app/project/endlessvendetta/page.tsx
+++ app/project/endlessvendetta/page.tsx
@@ -1,4 +1,6 @@
 import ProjectCarousel from "@/app/components/ProjectCarousel";
+import { CppSVG, UnrealEngineSVG, WindowsSVG } from "@/app/components/SVGs";
+import { Chip } from "@heroui/react";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -130,6 +132,86 @@ const EndlessVendettaProject = () => {
           },
         ]}
       />
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 p-4">
+        <div className="sm:place-self-end">
+          <h2 className="sm:text-3xl text-2xl sm:text-right">
+            Project Features
+          </h2>
+          <div className="flex flex-wrap gap-1 py-3 w-full">
+            <Chip
+              size="sm"
+              radius="sm"
+              variant="shadow"
+              className="flex gap-1"
+              color={"primary"}
+              startContent={<UnrealEngineSVG />}
+            >
+              Unreal Engine 5.1
+            </Chip>
+            <Chip
+              size="sm"
+              radius="sm"
+              variant="shadow"
+              className="flex gap-1"
+              color={"secondary"}
+              startContent={<CppSVG />}
+            >
+              C++
+            </Chip>
+            <Chip
+              size="sm"
+              radius="sm"
+              variant="shadow"
+              className="flex gap-1"
+              color={"default"}
+              startContent={<WindowsSVG />}
+            >
+              Windows
+            </Chip>
+          </div>
+          <ul className="sm:text-right">
+            <li>First Person Shooter</li>
+            <li>Complex Gun Play</li>
+            <li>Weapon Attachments</li>
+            <li>Open World</li>
+            <li>Mantling System</li>
+            <li>Bounties</li>
+            <li>Enemy AI</li>
+            <li>Spatial Inventory</li>
+            <li>Dialog System</li>
+            <li>Gadgets</li>
+          </ul>
+        </div>
+        <div>
+          <h2 className="sm:text-3xl text-2xl">Description</h2>
+          <p>
+            <br />
+            Crime, violence and betrayal runs rampant throughout the solar
+            system. Take on bounties as you hunt to kill the one who murdered
+            your family, and left you for dead.
+            <br />
+            <br />
+            Endless Vendetta is an realistic FPS game that features both
+            bounties that progress the main story and boast high payouts, or
+            exploration of the world.
+            <br />
+            <br />
+            Endless Vendetta is a 3rd year university project that was created
+            by a team of 12 people with different specialties over the course of
+            6 months.
+          </p>
+        </div>
+        <div className="sm:col-span-2 col-span-1">
+          <div className="flex justify-center">
+            <iframe
+              frameBorder="0"
+              src="https://itch.io/embed/2695551?dark=true"
+              width="552"
+              height="167"
+            />
+          </div>
+        </div>
+      </div>
     </>
   );
 };

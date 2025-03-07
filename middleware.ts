201bc6bfcc9baeb1fc9d35a857ca8eaa42631a4f
Philip White <philip@philipwhite.dev>
2025-03-07 21:55:05 +0000

    Update Backend for Proper Logging Format

--- middleware.ts
+++ middleware.ts
@@ -2,10 +2,19 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
+export function middleware(req: NextRequest) {
   if (process.env.NODE_ENV !== "production") return NextResponse.next();
 
-  const timestamp = new Date().toLocaleString();
-  console.log(`[${timestamp}] ${request.method} ${request.nextUrl.pathname}`);
+  const timestamp = new Date().toISOString();
+  console.log(
+    `[${timestamp}] ${req.method} ${req.nextUrl.pathname} for ${getTrueIP(req)}`
+  );
   return NextResponse.next();
+}
+
+function getTrueIP(req: NextRequest) {
+  const xForwardedFor = req.headers.get("x-forwarded-for");
+  return xForwardedFor
+    ? xForwardedFor.split(",")[0]?.replace("::ffff:", "")
+    : req.headers.get("x-real-ip")?.replace("::ffff:", "");
 }
